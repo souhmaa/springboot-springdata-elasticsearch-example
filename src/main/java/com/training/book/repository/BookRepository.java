19d77ce907d2f438605610b4006d738ce161ff1e
Souheil MAALAOUI <maalaoui.ensi@gmail.com>
2018-07-11 04:40:16 -0700

    First commit

---
+++ src/main/java/com/training/book/repository/BookRepository.java
@@ -0,0 +1,16 @@
+package com.training.book.repository;
+
+import com.training.book.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+public interface BookRepository extends ElasticsearchRepository<Book, String> {
+
+    Page<Book> findByAuthor(String author, Pageable pageable);
+
+    List<Book> findByTitle(String title);
+
+}
